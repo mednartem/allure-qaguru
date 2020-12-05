285dd1abf14cb07c1dab2ba9f9d6958301c3d7c4
mednartem <mednartem@yandex.ru>
2020-12-05 15:09:07 +0300

    remove excess import

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -4,7 +4,6 @@ import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
